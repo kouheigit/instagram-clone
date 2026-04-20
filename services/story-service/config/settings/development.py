1f2b713d5d8dc9744189884c03055bd2b4e0f49d
kouheigit <ookawakouhei777@gmail.com>
2026-04-20 20:38:30 +0900

    Add frontend CSRF trusted origins for dev

--- services/story-service/config/settings/development.py
+++ services/story-service/config/settings/development.py
@@ -1,5 +1,6 @@
 from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
 CORS_ALLOW_CREDENTIALS = True
--- services/user-service/config/settings/development.py
+++ services/user-service/config/settings/development.py
@@ -4,7 +4,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
 CORS_ALLOW_CREDENTIALS = True
 
 # 開発用: メール出力をコンソールへ
